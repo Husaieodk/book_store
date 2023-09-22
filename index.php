1f955131b4cf48757f574592defc472807b3b537
Husaieodk <irshadhmi98@gmail.com>
2023-09-21 23:06:45 -0700

    updated_first

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light " style="background-color: rgb(255, 255, 255);">
+            <div class="container px-4 px-lg-5"style=" padding: 0.3rem;">
+                <a class="navbar-brand" href="#!">Digitel Library</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Service</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact Us</a></li>
+                        
+                    </ul>
+                    <form class="d-flex " style="gap: 0.5rem;">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            My Book 
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                        <button class="btn btn-outline-dark " type="submit">
+                          
+                           <a href="login.php" style="text-decoration: none; color: black;" class="btn-log"> Log in</a>
+                        </button>
+                        <button class="btn btn-outline-dark" type="submit">
+                            <a href="Signup.php" style="text-decoration: none; color: black;"> Sign Up</a>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Digital</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">WA digital library, also called an online library, an internet library, a digital repository, 
+                        a library without walls, or a digital collection is an online database of digital objects that can include text, still images, audio, video, digital documents, or other digital media formats or a library accessible through the internet. </p>
+                </div>
+            </div>
+        </header>
+          
+         <div class="container text-center">
+         <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+            <div class="col">
+            <div class="p-3">
+            <div class="d-flex">
+            <div>All Books</div>
+               <div>Digital Book</div>
+               <div>Physical Book</div>
+            </div>
+            </div>
+            </div>
+            <div class="col">
+            <div class="p-3"></div>
+            </div>
+            <div class="col">
+            <div class="p-3"></div>
+            </div>
+            <div class="col">
+            <div class="p-3"></div>
+            </div>
+            <div class="col">
+            <div class="p-3">
+            <div class=" d-flex " style="align-items: end;gap:0.5rem"><div>
+                        <input type="text" id="enableTextInput1" class="form-control" name="news" autocomplete="off" placeholder="Search">
+                         
+                        </div>
+                        <div>
+                        <button type="submit" class="btn btn-primary mt-2"  name="submit">Search</button>
+                              
+                        </div>
+                             
+                                   
+               </div>
+            </div>
+            </div>
+         </div>
+         </div>   
+            
+        <!-- Section-->
+        <section class="">
+            <div class="container px-4 px-lg-5 mt-3">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    <!-- Product price-->
+                                    $40.00 - $80.00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="view.php">View options</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Special Item</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    <span class="text-muted text-decoration-line-through">$20.00</span>
+                                    $18.00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Sale Item</h5>
+                                    <!-- Product price-->
+                                    <span class="text-muted text-decoration-line-through">$50.00</span>
+                                    $25.00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- most view -->
+        
+        <div class="container border-radius:1rem"  >
+            <div class="text-center">
+            <h4>Views books</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis voluptates tenetur natus voluptatibus odit? Cumque, doloribus possimus quas voluptatum vero, quis id, quo adipisci provident sapiente delectus numquam minima ipsa?</p>
+        
+            </div>
+        <table class=" table  table-secondary table-striped-columns" >
+             <tr>
+                <th>irsa</th>
+                <th>erj</th>
+                <th>fvk</th>
+             </tr>
+             <tr>
+                <td>rfgj</td>
+                <td>erj</td>
+                <td>fewj</td>
+             </tr>
+          </table>
+            </div>       
+        <!-- most view -->
+
+        <!-- Footer-->
+        
+        <footer class="py-5 bg-dark">
+            <div style="background-color:#212529; height :15rem;">
+            
+                <div class="d-flex justify-content-center  container" style="gap: 8rem;  ">
+                    <div >
+                        <hr>
+                      <ul class="list-group list-group-flush ul-style mt-5" style="list-style-type: none;">
+                      <h3 class="footer-text">Digital Library</h3>
+                       &copy; 2020-<?php echo date("Y"); ?>
+                    
+                      </ul>
+                     </div>
+                      <div>
+                      <ul class="list-group list-group-flush mt-5" style="list-style-type: none;">
+                      <h4 class="footer-text">Menu</h4>
+                      <li class="footer-text ">About Us</li>
+                      <li class="footer-text">Contact</li>
+                      <li class="footer-text ">Terms and condition</li>
+                      <li class="footer-text">Privacy Policy</li>
+                      </ul>
+                        </ul>
+                      </div>
+                      <div>
+                      
+                      <ul class="list-group list-group-flush mt-5" style="list-style-type: none;">
+                      <h4 class="footer-text">Follow us</h4> 
+                      <li class="footer-text ">Instagram</li>
+                      <li class="footer-text ">Facebook</li>
+                      <li class="footer-text ">Twitter</li>
+                      <li class="footer-text ">Linkedin</li>
+                      <li class="footer-text ">Mail</li>
+                      </ul>
+                          </ul>
+                      </div>
+                      <div>
+                         <div class="mt-5">
+                          <h5 class="footer-text">News</h5>
+                          <input type="text" id="enableTextInput1" class="form-control" name="news" autocomplete="off" placeholder="Enter your Mail">
+                         
+                                  <button type="submit" class="btn btn-primary mt-2"  name="submit">subscribe</button>
+                                       
+                         </div>
+                      </div>
+                </div>
+                </div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ view.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light " style="background-color: rgb(255, 255, 255);">
+            <div class="container px-4 px-lg-5"style=" padding: 0.3rem;">
+                <a class="navbar-brand" href="#!">Digitel Library</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Service</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact Us</a></li>
+                        
+                    </ul>
+                    <form class="d-flex " style="gap: 0.5rem;">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            My Book 
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                        <button class="btn btn-outline-dark " type="submit">
+                          
+                           <a href="login.php" style="text-decoration: none; color: black;" class="btn-log"> Log in</a>
+                        </button>
+                        <button class="btn btn-outline-dark" type="submit">
+                            <a href="Signup.php" style="text-decoration: none; color: black;"> Sign Up</a>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </body>
+</html>        
